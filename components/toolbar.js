f08cea2a31dcbf65ec3d139adbdff9c7a8c8916b
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2021-03-02 13:45:09 +0200

    fix github link to _blank

--- components/toolbar.js
+++ components/toolbar.js
@@ -4,18 +4,16 @@ import styles from '../styles/Toolbar.module.css';
 export const Toolbar = () => {
   const router = useRouter();
 
+  const onGithubLinkClick = () => {
+    window.open('https://github.com/created-with-love', '_blank');
+  };
+
   return (
     <div className={styles.main}>
       <div onClick={() => router.push('/')}>Home</div>
       <div onClick={() => router.push('/feed/1')}>Feed</div>
       <div onClick={() => router.push('/eom')}>EOM</div>
-      <div
-        onClick={() =>
-          (window.location.href = 'https://github.com/created-with-love')
-        }
-      >
-        Github
-      </div>
+      <div onClick={onGithubLinkClick}>Github</div>
     </div>
   );
 };
